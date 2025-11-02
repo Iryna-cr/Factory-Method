a7947be0a5d595d7aeafeb4f619a31d15aedf3c9
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 20:25:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class FlowerShop
+{
+    public virtual string CreateFlower() => "Троянда ";
+}
+
+class TulipShop : FlowerShop
+{
+    public override string CreateFlower() => "Тюльпан ";
+}
+
+class Program
+{
+    static void Main()
+    {
+        FlowerShop roseShop = new FlowerShop();
+        FlowerShop tulipShop = new TulipShop();
+
+        Console.WriteLine(roseShop.CreateFlower());
+        Console.WriteLine(tulipShop.CreateFlower());
+    }
+}
+
+
+
